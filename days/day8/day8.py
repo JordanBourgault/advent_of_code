aa83e5aa789241896fd376bcd1df389bb6015edc
JordanBourgault <jordanbourgault@live.fr>
2024-12-09 09:08:47 -0500

    cleanup with numpy

--- days/day8/day8.py
+++ days/day8/day8.py
@@ -1,4 +1,5 @@
 from math import lcm
+import numpy as np
 
 with open('days/day8/input_test.txt') as f:
     data = [list(line.strip()) for line in f]
@@ -13,75 +14,73 @@ def get_antennas(data):
                 continue
             
             positions = antennas.get(val, [])
-            positions.append((i, j))
+            positions.append(np.array([i, j]))
             antennas[val] = positions
     return antennas
 
 antenna_data = get_antennas(data)
 
-def subtract(pos_a, pos_b):
-    return (pos_a[0] - pos_b[0], pos_a[1] - pos_b[1])
-
-def add(pos_a, pos_b):
-    return (pos_a[0] + pos_b[0], pos_a[1] + pos_b[1])
-
-def invert(vector):
-    return multiply(-1, vector)
-
-def multiply(a, vector):
-    return (a*vector[0], a*vector[1])
-
-def div(a, vector):
-    return (vector[0]//a, vector[1]//a)
 
 def bigger_or_equal(a, vector):
-    return a >= vector[0] or a >= vector[1]
+    return a >= max(vector)
+
 
 def check_bounds(map_bounds, pos):
-    return 0 <= pos[0] <= map_bounds[0] and 0 <= pos[1] <= map_bounds[1]
+    return 0 <= pos[0] < map_bounds[0] and 0 <= pos[1] < map_bounds[1]
+
 
 def check_harmonics(antinodes, pos, vector):
-    i = 0
+    i = 1
     while True:
-        node = add(pos, multiply(i, vector))
+        node = pos - i * vector
         if check_bounds(map_bounds, node):
-            antinodes.add(node)
+            antinodes.add(tuple(node))
         else:
             break
         i += 1
 
 
-def get_antinodes(antinodes, data, harmonics=False):
+def propagate_point(nodes, pos, vector, harmonics):
+    antinode_a = pos - vector
+    if check_bounds(map_bounds, antinode_a):
+        nodes.add(tuple(antinode_a))
+        if harmonics and check_harmonics:
+            check_harmonics(nodes, pos, vector)
+
+
+def get_antinodes(nodes, data, harmonics=False):
     for i, pos_a in enumerate(data):
         for j, pos_b in enumerate(data):
             if i == j:
                 continue
-            vector = subtract(pos_a, pos_b)
-            print('AAAAAAAa')
-            print(vector)
-            mul = lcm(*vector)
-            if not bigger_or_equal(mul, vector):
-                vector = div(lcm(*vector), vector)
-            print(vector)
+            vector = pos_a - pos_b
 
-            antinode_a = subtract(pos_a, invert(vector))
-            if check_bounds(map_bounds, antinode_a):
-                antinodes.add(antinode_a)
-                if check_harmonics:
-                    check_harmonics(antinodes, pos_a, invert(vector))
-            antinode_b = subtract(pos_b, vector)
-            if check_bounds(map_bounds, antinode_b):
-                antinodes.add(antinode_b)
-                if check_harmonics:
-                    check_harmonics(antinodes, pos_a, vector)
-                
+            propagate_point(nodes, pos_a, -vector, harmonics)
+            propagate_point(nodes, pos_b, vector, harmonics)
 
 
 antinodes = set()
+antinodes_harmonics = set()
+
 for key, values in antenna_data.items():
-    get_antinodes(antinodes, antenna_data[key], True)
+    get_antinodes(antinodes, antenna_data[key], False)
+    get_antinodes(antinodes_harmonics, antenna_data[key], harmonics=True)
     if len(values):
         for value in values:
-            antinodes.add(value)
+            antinodes_harmonics.add(tuple(value))
     
-print(len(antinodes))
+
+print('Part1:', len(antinodes))
+print('Part2:', len(antinodes_harmonics))
+
+
+def build_grid(map_bounds, antinodes):
+    grid = [['.' for _ in range(map_bounds[1])] for _ in range(map_bounds[0])]
+    for antinode in antinodes:
+        grid[antinode[0]][antinode[1]] = '#'
+    
+    with open('output.txt', 'w') as f:
+        for row in grid:
+            f.write(''.join(row) + '\n')
+
+build_grid(map_bounds, antinodes)
