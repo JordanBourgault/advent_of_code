3a69dee2f749bbd680f90ccdecd78a6e2f57e7ed
JordanBourgault <jordanbourgault@live.fr>
2024-12-13 16:35:35 -0500

    fix

--- 2015/day6/day6.py
+++ 2015/day6/day6.py
@@ -12,27 +12,27 @@ for i, instruction in enumerate(instructions):
     instruction = instruction[0]
     coords = {complex(i, j) for i in range(int(instruction[1]), int(instruction[3])+1) for j in range(int(instruction[2]), int(instruction[4])+1)}
     if instruction[0] == 'turn on':
-        # on |= coords
-        # off -= coords
+        on |= coords
+        off -= coords
         for coord in coords:
             lights_value[coord] += 1
 
     elif instruction[0] == 'turn off':
-        # off |= coords
-        # on -= coords
+        off |= coords
+        on -= coords
         for coord in coords:
             if lights_value[coord] > 0:
                 lights_value[coord] -= 1
     else:
-        # temp_on = coords & on
-        # temp_off = coords & off
-        # on |= temp_off
-        # off -= temp_off
-        # off |= temp_on
-        # on -= temp_on
+        temp_on = coords & on
+        temp_off = coords & off
+        on |= temp_off
+        off -= temp_off
+        off |= temp_on
+        on -= temp_on
         
         for coord in coords:
             lights_value[coord] += 2
 
-# print('Part 1:', len(on))
+print('Part 1:', len(on))
 print('Part 2:', sum(lights_value.values()))
