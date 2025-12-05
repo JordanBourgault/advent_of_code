078b90c940993dbf987c4617e9804daa68dd7cd2
jobou558 <jobou558@ulaval.ca>
2025-12-05 11:14:35 -0500

    cleanup

--- 2025/day5/day5.py
+++ 2025/day5/day5.py
@@ -2,39 +2,31 @@ with open("2025/day5/input.txt") as f:
     fresh_ranges, ingredients = f.read().split('\n\n')
 
 fresh = [list(map(int, (r.split('-')))) for r in fresh_ranges.split('\n')]
+sorted_fresh = sorted(fresh, key=lambda x: x[0])
+offset = 0
+for i in range(len(sorted_fresh)):
+    i += offset
+    try:
+        # Start of next range is contained in current range
+        if sorted_fresh[i][0] <= sorted_fresh[i+1][0] <= sorted_fresh[i][1]:
+            # Next range finishes outside current range
+            if sorted_fresh[i+1][1] > sorted_fresh[i][1]:
+                sorted_fresh[i] = [sorted_fresh[i][0], sorted_fresh[i+1][1]]
+                sorted_fresh.pop(i+1)
+                offset -= 1
+            # Next range is within current range
+            else:
+                sorted_fresh.pop(i+1)
+                offset -= 1
+    except IndexError:
+        pass
 
 good_ingredients = 0
 for ingredient in list(map(int, ingredients.strip().split('\n'))):
-    if any([f[0] <= ingredient <= f[1] for f in fresh]):
+    if any([f[0] <= ingredient <= f[1] for f in sorted_fresh]):
         good_ingredients += 1
         continue
 
 print('Part 1: ', good_ingredients)
-
-
-len_strikes = 0
-while True:
-    replaced = False
-    last_len = len(fresh)
-    for i, fresh_range in enumerate(fresh):
-        for j, sub_range in enumerate(fresh):
-            if i == j:
-                continue
-            if sub_range[0] <= fresh_range[0] <= sub_range[1]:
-                if fresh_range[1] >= sub_range[1]:
-                    fresh[j] = [sub_range[0], fresh_range[1]]
-                    fresh.pop(i)
-                    replaced = True
-                    break
-                else:
-                    fresh.pop(i)
-                    replaced = True
-                    break
-        if replaced:
-            break
-    current_len = len(fresh)
-    if last_len == current_len:
-        break
-
-total_fresh = sum([el[1] - el[0] + 1 for el in fresh])
+total_fresh = sum([el[1] - el[0] + 1 for el in sorted_fresh])
 print('Part 2: ', total_fresh)
